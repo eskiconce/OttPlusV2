1c485ee26ea1fca378e03496e7301946cb2570a6
Edmundo Cuevas <50032838+eskiconce@users.noreply.github.com>
2026-09-24 13:11:42 -0300

    feat: add ActivationActivity

---
+++ app/src/main/java/com/ott/tv/presentation/activation/ActivationActivity.kt
@@ -0,0 +1,13 @@
+package com.ott.tv.presentation.activation
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.ott.tv.R
+
+class ActivationActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activation)
+    }
+}
